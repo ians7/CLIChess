213cc967fe9623308f7001be9abffced7a35e4a0
ian <sawyerij@rose-hulman.edu>
2024-05-28 12:20:36 -0400

    Pawn captures work, fixed input parsing to use regex

--- main/chessGame.go
+++ main/chessGame.go
@@ -4,8 +4,7 @@ import (
     "fmt"
     "os"
     "bufio"
-	"os/exec"
-	"strings"
+	"regexp"
 )
 
 type Piece struct {
@@ -13,28 +12,29 @@ type Piece struct {
     icon int
     color string
     spacesCanMove int16
+	canEnPassant bool
 }
 
 var G = "\u001b[38;5;243m"
 var W = "\u001b[38;5;15m"
 var B = "\u001b[38;5;232m"
 var Br = "\u001b[38;5;94m"
-
-var blackKing = Piece{'K', '\u2654', G, 1}
-var blackQueen = Piece{'Q', '\u2655', G, 7}
-var blackRook = Piece{'R', '\u2656', G, 7}
-var blackKnight = Piece{'N', '\u2658', G, 3}
-var blackBishop = Piece{'B', '\u2657', G, 7}
-var blackPawn = Piece{'P', '\u2659', G, 2}
-var whiteKing = Piece{'K', '\u2654', W, 1}
-var whiteQueen = Piece{'Q', '\u2658', W, 7}
-var whiteRook = Piece{'R', '\u2656', W, 7}
-var whiteKnight = Piece{'N', '\u2658', W, 3}
-var whiteBishop = Piece{'B', '\u2657', W, 7}
-var whitePawn = Piece{'P', '\u2659', W, 2}
-var emptySquare = Piece{'0', ' ', B, 0}
+var blackKing = Piece{'K', '\u2654', G, 1, false}
+var blackQueen = Piece{'Q', '\u2655', G, 7, false}
+var blackRook = Piece{'R', '\u2656', G, 7, false}
+var blackKnight = Piece{'N', '\u2658', G, 3, false}
+var blackBishop = Piece{'B', '\u2657', G, 7, false}
+var blackPawn = Piece{'P', '\u2659', G, 2, false}
+var whiteKing = Piece{'K', '\u2654', W, 1, false}
+var whiteQueen = Piece{'Q', '\u2658', W, 7, false}
+var whiteRook = Piece{'R', '\u2656', W, 7, false}
+var whiteKnight = Piece{'N', '\u2658', W, 3, false}
+var whiteBishop = Piece{'B', '\u2657', W, 7, false}
+var whitePawn = Piece{'P', '\u2659', W, 2, false}
+var emptySquare = Piece{'0', ' ', B, 0, false}
 
 func initializeBoard() [8][8]Piece {
+
     board := [8][8]Piece{
 		{blackRook, blackKnight, blackBishop, blackQueen, blackKing, blackBishop, blackKnight, blackRook},
 		{blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn, blackPawn},
@@ -61,28 +61,40 @@ func printBoard(board [8][8]Piece) {
     }
 }
 
-func movePiece(input string, whiteTurn bool, board [8][8]Piece) ([8][8]Piece, bool) {
-	var pieceType byte 
-	capture := false
-	if strings.Contains(input, "x") {
-		capture = true
-	}
-	var capturingPieceFile int16 = 0
-	if input[0] >= 97 && input[0] <= 104 {
-		if capture {
-			capturingPieceFile = int16(input[0] - '0' - 48)
+func parseInput(input string, pieceType* int, capture* bool, capturingPieceFile* int) bool {
+	if match, err := regexp.MatchString(`([a-hNKQBR])?([a-h1-8])x[a-h][1-8]\n`, input) ; err == nil && match {
+		*capture = true
+		*capturingPieceFile = int(input[2] - '0' - 49)
+		fmt.Println(input[2], *capturingPieceFile, *capture)
+		if input[0] >= 97 && input[0] <= 104 {
+			*pieceType = 'P'
+			*capturingPieceFile = int(input[0] - '0' - 49)
+		} else {
+			*pieceType = int(input[0])
 		}
-		pieceType = 'P'
-	} else if input[0] == 'N' || input[0] == 'K' || input[0] == 'Q' || input[0] == 'B' || input[0] == 'R' {
-		pieceType = input[0]
+		return true
+	} else if match, err := regexp.MatchString(`[a-h][1-8]\n`, input) ; err == nil && match {
+		*pieceType = 'P'
+		*capture = false
+		return true
 	} else {
-		fmt.Println("Please input valid chess notation")
-		return board, false;
+		fmt.Println("Please input valid chess notation", input)
+		fmt.Println(match, err)
+		return false
 	}
+}
 
+func movePiece(input string, whiteTurn bool, board [8][8]Piece) ([8][8]Piece, bool) {
+	pieceType := 0
+	capture := false
+	capturingPieceFile := 0
+	if (!parseInput(input, &pieceType, &capture, &capturingPieceFile)) {
+		return board, false
+	}
+	fmt.Println(input[2], capturingPieceFile, capture, pieceType)
 	row := int16(8 - (input[len(input) - 2] - '0'))
 	file := int16((input[len(input) - 3] - '0') - 49)
-	fmt.Println(file, row)
+	
 	color := "" 
 	if whiteTurn {
 		color = W
@@ -95,9 +107,8 @@ func movePiece(input string, whiteTurn bool, board [8][8]Piece) ([8][8]Piece, bo
 			if board[i][j].color == color && int16(board[i][j].ID) == int16(pieceType) {
 				if pieceType == 'P' {
 					if whiteTurn {
-						if capture && j == capturingPieceFile { 
+						if capture && j == int16(capturingPieceFile) { 
 							if ((file == j + 1 && row == i - 1) || (file == j - 1 && row == i - 1)) && board[row][file].color == G {
-								fmt.Println(j, capturingPieceFile)
 								board[row][file] = board[i][j]
 								board[i][j] = emptySquare
 								return board, true
@@ -121,7 +132,7 @@ func movePiece(input string, whiteTurn bool, board [8][8]Piece) ([8][8]Piece, bo
 								return board, false
 							}
 						} 
-						if row - i <= board[i][j].spacesCanMove && file == j {
+						if row - i <= board[i][j].spacesCanMove && file == j && board[row][file].color == B && capturingPieceFile == 0{
 							board[row][file] = board[i][j]
 							board[i][j] = emptySquare
 							board[row][file].spacesCanMove = 1
@@ -147,6 +158,7 @@ func movePiece(input string, whiteTurn bool, board [8][8]Piece) ([8][8]Piece, bo
 			}
 		}
 	}
+    fmt.Println("failed at end of function")
 	return board, false
 }
 
@@ -173,7 +185,6 @@ func main() {
 		if success {
 			whiteTurn = !whiteTurn
 		}
-		exec.Command("clear")
     }
 
 }
--- main/chess_test.go
+++ main/chess_test.go
@@ -1,15 +1,152 @@
 package main
 
 import "testing"
+import "fmt"
 
-func TestPawnMovement(t *testing.T) {
-	
+func TestBasicPawnMovement(t *testing.T) {
 	board := initializeBoard()
+	success := false
 	whiteTurn := true
-	movePiece("e4", whiteTurn, board)
-	if board['e' - 97][4].ID != 'P' {
-		t.Errorf("Pawn did not move correctly.")
+	passedAll := true
+	board, success = movePiece("e4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("e5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("e5\n", whiteTurn, board)
+	if success == true {
+		t.Errorf("\u001b[31m" + "should not be able to move to e5")
+	}
+	board, success = movePiece("a3\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a3")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("f5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed f5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("a4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a4")
+		passedAll = false
 	}
 	printBoard(board)
+	if passedAll {
+		fmt.Println("\u001b[32m" + "PASSED basic pawn movement.")
+	}
+}
 
+func TestMultiplePossiblePawnCaptures(t *testing.T) {
+	
+	// f takes
+
+	board := initializeBoard()
+	success := false
+	passedAll := true
+	whiteTurn := true
+	board, success = movePiece("e4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("e5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("d4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed d4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("a6\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a6")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("f4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed f4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("a5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("fxe5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed fxe5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	printBoard(board)
+		
+	// d takes
+	board = initializeBoard()
+	success = false
+	whiteTurn = true
+	board, success = movePiece("e4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("e5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed e5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("d4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed d4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("a6\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a6")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("f4\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed f4")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("a5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed a5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	board, success = movePiece("dxe5\n", whiteTurn, board)
+	if success == false {
+		t.Errorf("\u001b[31m" + "Failed dxe5")
+		passedAll = false
+	}
+	whiteTurn = !whiteTurn
+	printBoard(board)
+	if passedAll {
+		fmt.Println("\u001b[32m" + "PASSED selecting capturing piece." + "\u001b[37m")
+	}
 }
