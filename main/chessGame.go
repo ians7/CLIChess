1b39dde526365b0e665c2dbca5dd77516444687b
ian <sawyerij@rose-hulman.edu>
2024-06-14 23:10:24 -0400

    created functions for king checks, captures with non-pawns now work

--- main/chessGame.go
+++ main/chessGame.go
@@ -76,7 +76,20 @@ func printBoard(board [8][8]Piece) {
 }
 
 func parseInput(input string, pieceType *int, capture *bool, capturingPieceFile *int) bool {
-	if match, err := regexp.MatchString(`^([a-hNKQBR])?([a-h1-8])x[a-h][1-8]\n$`, input); err == nil && match {
+	if match, err := regexp.MatchString(`^([a-hNKQBR])[a-h]x?[a-h][1-8]\n$`, input); err == nil && match {
+		fmt.Println("have not implemeted disambiguation")
+		return false
+	} else if match, err := regexp.MatchString(`^([a-hNKQBR])[1-8]x?[a-h][1-8]\n$`, input); err == nil && match {
+		fmt.Println("have not implemeted disambiguation")
+		return false
+	} else if match, err := regexp.MatchString(`^([a-hNKQBR])[1-8]x?[a-h][1-8]\n$`, input); err == nil && match {
+		fmt.Println("have not implemeted disambiguation")
+		return false
+	} else if match, err := regexp.MatchString(`^([a-hNKQBR])[a-h][1-8]x?[a-h][1-8]\n$`, input); err == nil && match {
+		fmt.Println("have not implemeted double disambiguation")
+		return false
+	}
+	if match, err := regexp.MatchString(`^[a-hNKQBR][a-h]?[1-8]?x[a-h][1-8]\n$`, input); err == nil && match {
 		*capture = true
 		*capturingPieceFile = int(input[2] - '0' - 49)
 		if input[0] >= 97 && input[0] <= 104 {
@@ -93,7 +106,7 @@ func parseInput(input string, pieceType *int, capture *bool, capturingPieceFile 
 		*pieceType = 'P'
 		return true
 	} else {
-		fmt.Println("Please input valid chess notation", input)
+		fmt.Println("Please input valid chess notation (in parse input)", input)
 		fmt.Println(match, err)
 		return false
 	}
@@ -102,6 +115,7 @@ func parseInput(input string, pieceType *int, capture *bool, capturingPieceFile 
 func checkPieceInWay(board [8][8]Piece, pieceRow int16, pieceFile int16, destRow int16, destFile int16) bool {
 	rowIterator := 1
 	fileIterator := 1
+	fmt.Println("pieceFile =", pieceFile, "destFile =", destFile)
 	if pieceRow > destRow {
 		rowIterator = -1
 	} else if pieceRow == destRow {
@@ -128,8 +142,13 @@ func checkPieceInWay(board [8][8]Piece, pieceRow int16, pieceFile int16, destRow
 	return false
 }
 
+func detectCheckOnKing(board [8][8]Piece) Piece {
+	fmt.Println("Have not implemented checks.")
+	return emptySquare
+}
+
 func whiteMovement(board [8][8]Piece, row int16, file int16, capture bool, capturingPieceFile int, pieceType int) ([8][8]Piece, bool) {
-	
+
 	for i := int16(0); i < 8; i++ {
 		for j := int16(0); j < 8; j++ {
 			if board[i][j].teamID == 0 && int16(board[i][j].pieceID) == int16(pieceType) {
@@ -146,7 +165,8 @@ func whiteMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 							board[i][j] = emptySquare
 							board[row][file] = emptySquare
 							return board, true
-						}					}
+						}
+					}
 					if i-row > 0 && i-row <= board[i][j].spacesCanMove && file == j && board[row][file].pieceID == '0' && capturingPieceFile == 0 {
 						board[i][j].canBeEnPassant = false
 						if math.Abs(float64(i-row)) == 2 {
@@ -157,11 +177,18 @@ func whiteMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 						board[row][file].spacesCanMove = 1
 						return board, true
 					}
-				}
-				if pieceType == 'K' {
-				}
-				if pieceType == 'Q' {
+				} else if pieceType == 'K' {
+					if checkPieceInWay(board, i, j, row, file) == true || math.Abs(float64(row-i)) > 1 || math.Abs(float64(j-file)) > 1 {
+						return board, false
+					}
 
+					if math.Abs(float64(row-i)) == math.Abs(float64(j-file)) || file-j == 0 || row-i == 0 {
+						board[row][file] = board[i][j]
+						board[i][j] = emptySquare
+						return board, true
+					}
+
+				} else if pieceType == 'Q' {
 					if checkPieceInWay(board, i, j, row, file) == true {
 						return board, false
 					}
@@ -170,20 +197,25 @@ func whiteMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 						board[row][file] = board[i][j]
 						board[i][j] = emptySquare
 						return board, true
-					} 
-				}
-				if pieceType == 'R' {
+					}
+				} else if pieceType == 'R' {
 
-				}
-				if pieceType == 'N' {
-
-					if (math.Abs(float64(row-i)) == 2 && math.Abs(float64(file-j)) == 1) || (math.Abs(float64(row-i)) == 1 && math.Abs(float64(file-j)) == 2) { 
+					if file-j == 0 || row-i == 0 {
+						if checkPieceInWay(board, i, j, row, file) == true {
+							return board, false
+						}
 						board[row][file] = board[i][j]
 						board[i][j] = emptySquare
 						return board, true
-					} 				
-				}
-				if pieceType == 'B' {
+					}
+				} else if pieceType == 'N' {
+
+					if (math.Abs(float64(row-i)) == 2 && math.Abs(float64(file-j)) == 1) || (math.Abs(float64(row-i)) == 1 && math.Abs(float64(file-j)) == 2) {
+						board[row][file] = board[i][j]
+						board[i][j] = emptySquare
+						return board, true
+					}
+				} else if pieceType == 'B' {
 
 					if (i%2 == j%2 && row%2 == file%2) || (i%2 != j%2 && row%2 != file%2) {
 						if checkPieceInWay(board, i, j, row, file) == true {
@@ -222,8 +254,8 @@ func blackMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 					}
 					if row-i > 0 && row-i <= board[i][j].spacesCanMove && file == j && board[row][file].pieceID == '0' && capturingPieceFile == 0 {
 						board[i][j].canBeEnPassant = false
-						fmt.Println("canBeEnPassant ->", math.Abs(float64(row - i)))
-						if math.Abs(float64(row - i)) == 2 {
+						fmt.Println("canBeEnPassant ->", math.Abs(float64(row-i)))
+						if math.Abs(float64(row-i)) == 2 {
 							board[i][j].canBeEnPassant = true
 						}
 						board[row][file] = board[i][j]
@@ -231,11 +263,17 @@ func blackMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 						board[row][file].spacesCanMove = 1
 						return board, true
 					}
-				}
-				if pieceType == 'K' {
+				} else if pieceType == 'K' {
+					if checkPieceInWay(board, i, j, row, file) == true || math.Abs(float64(row-i)) > 1 || math.Abs(float64(j-file)) > 1 {
+						return board, false
+					}
 
-				}
-				if pieceType == 'Q' {
+					if math.Abs(float64(row-i)) == math.Abs(float64(j-file)) || file-j == 0 || row-i == 0 {
+						board[row][file] = board[i][j]
+						board[i][j] = emptySquare
+						return board, true
+					}
+				} else if pieceType == 'Q' {
 
 					if checkPieceInWay(board, i, j, row, file) == true {
 						return board, false
@@ -246,19 +284,23 @@ func blackMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 						board[i][j] = emptySquare
 						return board, true
 					}
-				}
-				if pieceType == 'R' {
-
-				}
-				if pieceType == 'N' {
-
-					if (math.Abs(float64(row-i)) == 2 && math.Abs(float64(file-j)) == 1) || (math.Abs(float64(row-i)) == 1 && math.Abs(float64(file-j)) == 2) { 
+				} else if pieceType == 'R' {
+					if file-j == 0 || row-i == 0 {
+						if checkPieceInWay(board, i, j, row, file) == true {
+							return board, false
+						}
 						board[row][file] = board[i][j]
 						board[i][j] = emptySquare
 						return board, true
-					} 				
-				}
-				if pieceType == 'B' {
+					}
+				} else if pieceType == 'N' {
+
+					if (math.Abs(float64(row-i)) == 2 && math.Abs(float64(file-j)) == 1) || (math.Abs(float64(row-i)) == 1 && math.Abs(float64(file-j)) == 2) {
+						board[row][file] = board[i][j]
+						board[i][j] = emptySquare
+						return board, true
+					}
+				} else if pieceType == 'B' {
 
 					if (i%2 == j%2 && row%2 == file%2) || (i%2 != j%2 && row%2 != file%2) {
 						if checkPieceInWay(board, i, j, row, file) == true {
@@ -269,7 +311,7 @@ func blackMovement(board [8][8]Piece, row int16, file int16, capture bool, captu
 							board[row][file] = board[i][j]
 							board[i][j] = emptySquare
 							return board, true
-						} 
+						}
 					}
 				}
 			}
@@ -333,6 +375,5 @@ func main() {
 			fmt.Println("Please input a valid move.")
 		}
 	}
-
 
 }
