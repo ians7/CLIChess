56e9dc6db7c6ad0a0e8231f4c4e8babfe9196e0a
ians7 <ijsaw1@outlook.com>
2024-07-04 01:58:10 -0400

    changed ip from localhost to my pc

--- src/serverApp/client.go
+++ src/serverApp/client.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	conn, conErr := net.Dial("tcp", ":20000")
+	conn, conErr := net.Dial("tcp", "192.168.254.183:20000")
 	if conErr != nil {
 		fmt.Println("Failed to connect to the server")
 		return
